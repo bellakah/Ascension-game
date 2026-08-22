6d0ead08e025a95540dd53debac9102b39484137
bellakah <137653378+bellakah@users.noreply.github.com>
2026-08-22 19:26:24 -0300

    chore: remove superseded layered wrapper

--- build/editorLayeredCanvasPostPlugin.ts
+++
@@ -1,9 +0,0 @@
-import { editorLayeredCanvasPlugin } from './editorLayeredCanvasPlugin'
-
-export function editorLayeredCanvasPostPlugin() {
-  const { enforce: _enforce, ...plugin } = editorLayeredCanvasPlugin()
-  return {
-    ...plugin,
-    name: 'ascension-editor-layered-canvas-normal',
-  }
-}
