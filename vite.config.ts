3c92529ce8d960f5978f356be2810b5641a04644
bellakah <137653378+bellakah@users.noreply.github.com>
2026-08-23 00:13:25 -0300

    feat(lighting): enable day night editor plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import { editorCombinedPerformancePlugin } from './build/editorCombinedPerformancePlugin'
+import { editorLightingPlugin } from './build/editorLightingPlugin'
 
 export default defineConfig({
   base: '/Ascension-game/',
-  plugins: [editorCombinedPerformancePlugin()],
+  plugins: [editorCombinedPerformancePlugin(), editorLightingPlugin()],
 })
